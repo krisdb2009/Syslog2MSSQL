15ef7869629b171d713adf6b2bd94ebc2a169e76
Dylan Bickerstaff <dylan@belowaverage.org>
2024-07-18 01:45:26 -0400

    Add project files.

---
+++ Syslog2MSSQL/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using SyslogDecode.Common;
+using SyslogDecode.Model;
+using SyslogDecode.Udp;
+using System.Net;
+using System.Reflection;
+
+namespace Syslog2MSSQL
+{
+    internal class Program
+    {
+        static SyslogUdpPipeline Pipeline = new(IPAddress.Any);
+        static SqlConnection SqlConnection = new(Environment.GetEnvironmentVariable("S2M_CONNECTIONSTRING"));
+        static void Main(string[] args)
+        {
+            Assembly self = Assembly.GetExecutingAssembly();
+            Console.WriteLine(
+                self.GetCustomAttribute<AssemblyTitleAttribute>().Title +
+                " v" +
+                self.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion
+            );
+            Console.WriteLine("Connecting to SQL...");
+            SqlConnection.Open();
+            Console.WriteLine("Starting listener...");
+            Pipeline.StreamParser.ItemProcessed += StreamParser_ItemProcessed;
+            Pipeline.Start();
+            Console.WriteLine("Running.");
+            Console.ReadLine();
+            Console.WriteLine("Stopping listener...");
+            Pipeline.Stop();
+            Console.WriteLine("Closing SQL connection...");
+            SqlConnection.Close();
+            Console.WriteLine("Stopped.");
+        }
+        private static void StreamParser_ItemProcessed(object? sender, ItemEventArgs<ParsedSyslogMessage> e)
+        {
+            SqlCommand cmd = SqlConnection.CreateCommand();
+            cmd.CommandText = "INSERT INTO [logs] ([time], [host], [severity], [facility], [application], [process], [message]) VALUES (@TIME@, @HOST@, @SEVERITY@, @FACILITY@, @APPLICATION@, @PROCESS@, @MESSAGE@)";
+            cmd.Parameters.AddWithValue("@TIME@", (object)e.Item.Header.Timestamp ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@HOST@", (object)e.Item.Header.HostName ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@SEVERITY@", (object)e.Item.Severity.ToString() ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@FACILITY@", (object)e.Item.Facility.ToString() ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@APPLICATION@", (object)e.Item.Header.AppName ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@PROCESS@", (object)e.Item.Header.ProcId ?? DBNull.Value);
+            cmd.Parameters.AddWithValue("@MESSAGE@", (object)e.Item.Message ?? DBNull.Value);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
